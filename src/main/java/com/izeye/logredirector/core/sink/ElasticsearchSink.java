91bc128d6314b118be6efc303baf1effc6731ca6
Johnny Lim <izeye@naver.com>
2016-07-18 16:42:38 +0900

    Log elapsed time for bulk requests

--- src/main/java/com/izeye/logredirector/core/sink/ElasticsearchSink.java
+++ src/main/java/com/izeye/logredirector/core/sink/ElasticsearchSink.java
@@ -132,8 +132,10 @@ public class ElasticsearchSink extends Sink {
 		public void run() {
 			int count = this.bulkRequestBuilder.numberOfActions();
 			log.debug("{} bulk requests will be sent.", count);
+			long startTimeInMillis = System.currentTimeMillis();
 			this.bulkRequestBuilder.get();
-			log.debug("{} bulk requests have been sent.", count);
+			long elapsedTimeInMillis = System.currentTimeMillis() - startTimeInMillis;
+			log.info("Elapsed time for {} bulk requests: {} ms", count, elapsedTimeInMillis);
 			markProcessed(count);
 		}
 		
